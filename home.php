63d90355c7bfcc2811d4dc10bcca624f2650236e
DREAMREAPER-hack <77731302+DREAMREAPER-hack@users.noreply.github.com>
2021-01-20 20:24:03 +0700

    uas2021
    untuk uas stikom 2021

---
+++ home.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SERVIS KENDARAAN</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.3.0/css/all.min.css">
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</head>
+<body>
+  <!--loader -->
+  <div class="bg-loader">
+    <div class="loader"></div>      
+  </div>
+
+  <!--header-->
+  <div class="medsos">
+    <div class="container">
+      <ul>
+        <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#"><i class="fab fa-facebook"></i></a></li>
+        <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+      </ul>
+    </div>
+  </div>
+  <header>
+    <div class="container">
+      <h1><a href="home.php">BERANDA</a></h1>
+      <ul>
+        <li class="active"><a href="home.php">HOME</a></li>
+        <li><a href="layananservis.php">LAYANAN SERVIS</a></li>
+        <li><a href="service.html">PANDUAN</a></li>
+        <li><a href="logout.php">LOGOUT</a></li>
+    </div>
+  </header>
+
+  <!--banner-->
+  <section class="banner">
+    <h2>SELAMAT DATANG DI SERVIS KENDARAAN</h2>
+
+  </section>
+
+  <!--layananservis-->
+  <section class="layananservis">
+    <dir class="container">
+      <h3>LAYANAN SERVIS</h3>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <strong>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</strong> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+    </dir>
+  </section>
+
+  <!--service-->
+  <section class="service">
+    <div class="container">
+      <h3>SERVICE</h3>
+      <div class="box">
+        <div class="col-4">
+          <div class="icon"><i class="fas fa-car"></i></div>
+          <h4>MOBIL APP</h4>
+        </div>
+        <div class="col-4">
+          <div class="icon"><i class="fas fa-globe"></i></div>
+          <h4>google</h4>
+        </div>
+        <div class="col-4">
+          <div class="icon"><i class="fas fa-motorcycle"></i></div>
+          <h4>developer</h4>
+        </div>
+        <div class="col-4">
+          <div class="icon"><i class="fas fa-chart-bar"></i></div>
+          <h4>oke</h4>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- footer -->
+  <footer>
+    <div class="container">
+      <small>copyright &copy; 2021 -servis kendaraan. All Rights Reserved </small>
+    </div>
+  </footer>
+
+  ><script type="text/javascript">
+    $(document).ready(function () {
+      $(".bg-loader").hide();   
+    })
+  </script>
+
+</body>
+</html>
+
+ 
+ <a href="logout.php" class="form-inline my-2 my-lg-0 btn btn-secondary">Logout</a> -->
